add9be18125dbe86deb6419a2c46b5d4722e40de
shyam <strikerallin1@gmail.com>
2021-04-02 14:10:59 +0530

    finished up to WaterJugProblem

--- src/graph/OliverAndTheGame.java
+++ src/graph/OliverAndTheGame.java
@@ -1,18 +1,15 @@
 package graph;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class OliverAndTheGame {
 
-    private static int kingIndex;
+    private static int time;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(reader.readLine());
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -21,53 +18,34 @@ public class OliverAndTheGame {
             int[] road = Arrays.stream(reader.readLine().split(" ")).limit(2).mapToInt(Integer::parseInt).toArray();
             adj.get(road[0]).add(road[1]);
         }
-        int[] sort = sort(N, adj);
+        time = 0;
+        int[] inTime = new int[N + 1], outTime = new int[N + 1];
+        dfs(1, inTime, outTime, adj);
         int Q = Integer.parseInt(reader.readLine());
         for (int i = 0; i < Q; i++) {
             int[] q = Arrays.stream(reader.readLine().split(" ")).limit(3).mapToInt(Integer::parseInt).toArray();
-            System.out.println(find(q[0], q[2], q[1], sort) ? "YES" : "NO");
+            if (!check(q[1], q[2], inTime, outTime) && !check(q[2], q[1], inTime, outTime)) {
+                System.out.println("NO");
+                continue;
+            }
+            if (q[0] == 0) {
+                System.out.println(check(q[2], q[1], inTime, outTime) ? "YES" : "NO");
+            } else if (q[0] == 1) {
+                System.out.println(check(q[1], q[2], inTime, outTime) ? "YES" : "NO");
+            }
         }
     }
 
-    private static boolean find(int direction, int src, int dst, int[] sort) {
-        int srcIndex = 0, dstIndex = 0;
-        for (int i = 1; i < sort.length; i++) {
-            if (sort[i] == src) srcIndex = i;
-            if (sort[i] == dst) dstIndex = i;
-        }
-        if (kingIndex < srcIndex) {
-            if (direction == 0) {
-                return dstIndex >= kingIndex && dstIndex < srcIndex;
-            }
-            return dstIndex > srcIndex;
-        }
-        if (direction == 0) {
-            return dstIndex <= kingIndex && dstIndex > srcIndex;
-        }
-        return dstIndex < srcIndex;
+    private static boolean check(int X, int Y, int[] inTime, int[] outTime) {
+        return inTime[Y] < inTime[X] && outTime[Y] > outTime[X];
     }
 
-    private static int[] sort(int n, ArrayList<ArrayList<Integer>> adj) {
-        int[] inDegree = new int[n + 1];
-        int[] result = new int[n + 1];
-        int ptr = 1;
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            for (int node : adj.get(i)) {
-                inDegree[node]++;
-            }
+    private static void dfs(int src, int[] inTime, int[] outTime, ArrayList<ArrayList<Integer>> adj) {
+        inTime[src] = ++time;
+        for (int neighbor : adj.get(src)) {
+            if (inTime[neighbor] == 0) dfs(neighbor, inTime, outTime, adj);
         }
-        for (int i = 1; i < n; i++) {
-            if (inDegree[i] == 0) q.add(i);
-        }
-        while (!q.isEmpty()) {
-            int current = q.remove();
-            if (current == 1) kingIndex = ptr;
-            result[ptr++] = current;
-            for (int neighbor : adj.get(current)) {
-                if (--inDegree[neighbor] == 0) q.add(neighbor);
-            }
-        }
-        return result;
+        outTime[src] = ++time;
     }
+
 }
---
+++ src/graph/WaterJugProblem.java
@@ -0,0 +1,83 @@
+package graph;
+
+import java.util.*;
+
+public class WaterJugProblem {
+    public static void main(String[] args) {
+        int a = 4, b = 3, target = 2;
+        solve(a, b, target);
+    }
+
+    private static void solve(int a, int b, int target) {
+        Set<State> visited = new HashSet<>();
+        Queue<State> q = new LinkedList<>();
+        ArrayList<State> result = new ArrayList<>();
+        boolean notSolvable = true;
+        q.add(new State(0, 0));
+        while (!q.isEmpty()) {
+            State curr = q.remove();
+            if (visited.contains(curr)) continue;
+            if (curr.a > a || curr.b > b || curr.a < 0 || curr.b < 0) continue;
+            result.add(curr);
+            visited.add(curr);
+
+            // Target State Reached
+            if (curr.a == target || curr.b == target) {
+                notSolvable = false;
+                if (curr.a == target) {
+                    if (curr.b != 0) result.add(new State(target, 0));
+                } else {
+                    if (curr.a != 0) result.add(new State(0, target));
+                }
+                result.forEach(System.out::println);
+                break;
+            }
+
+            // Fill Jug2 and Jug1
+            q.add(new State(curr.a, b));
+            q.add(new State(a, curr.b));
+
+            // Pour Jug2 -> Jug1 and vice versa
+            for (int ap = 0; ap <= Math.max(a, b); ap++) {
+                int c = curr.a - ap;
+                int d = curr.b + ap;
+                if (d == b || c == 0) q.add(new State(c, d));
+                c = curr.a + ap;
+                d = curr.b - ap;
+                if (c == a || d == 0) q.add(new State(c, d));
+            }
+
+            // Empty Jug2 and Jug1
+            q.add(new State(curr.a, 0));
+            q.add(new State(0, curr.b));
+        }
+        if (notSolvable) System.out.println("NO SOLUTION EXISTS!");
+    }
+
+    private static class State {
+        int a, b;
+
+        State(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof State)) return false;
+            State state = (State) o;
+            return a == state.a && b == state.b;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + a + ", " + b + ")";
+        }
+    }
+}
