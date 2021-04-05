dc0b5183bb6446b146e9478b6e0441666c4476ae
shyam <strikerallin1@gmail.com>
2021-04-05 14:22:37 +0530

    Finished Trie Section

---
+++ src/trie/FindShortestUniquePrefixOfEachWord.java
@@ -0,0 +1,66 @@
+package trie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindShortestUniquePrefixOfEachWord {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        String[] words = {"zebra", "dog", "duck", "dove"};
+        for (String word : words) trie.insert(word);
+        trie.prefixAll();
+    }
+
+    private static class Trie {
+        private final TrieNode root;
+
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                curr.frequency++;
+                curr.children.putIfAbsent(word.charAt(i), new TrieNode());
+                curr = curr.children.get(word.charAt(i));
+            }
+            curr.frequency++;
+        }
+
+        String prefix(String word) {
+            TrieNode curr = root;
+            int i = 0;
+            while (i < word.length() && curr.frequency > 1) {
+                curr = curr.children.get(word.charAt(i++));
+            }
+            return word.substring(0, i);
+        }
+
+        void prefixAll() {
+            prefixAllUtil(root, "");
+        }
+
+        void prefixAllUtil(TrieNode curr, String prefix) {
+            if (curr == null) return;
+            if (curr.frequency == 1) {
+                System.out.print(prefix + " ");
+                return;
+            }
+            for (char child : curr.children.keySet()) {
+                prefixAllUtil(curr.children.get(child), prefix + child);
+            }
+        }
+
+        private static class TrieNode {
+            Map<Character, TrieNode> children;
+            int frequency;
+
+            TrieNode() {
+                children = new HashMap<>();
+                frequency = 0;
+            }
+        }
+    }
+}
---
+++ src/trie/WordBreakProblem.java
@@ -0,0 +1,60 @@
+package trie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordBreakProblem {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        String[] words = {"i", "like", "sam", "sung", "samsung", "mobile", "ice",
+                "cream", "icecream", "man", "go", "mango"};
+        for (String word : words) trie.insert(word);
+        System.out.println(trie.wordBreak("ilikesamsung"));
+    }
+
+    private static class Trie {
+        TrieNode root;
+
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                curr.children.putIfAbsent(word.charAt(i), new TrieNode());
+                curr = curr.children.get(word.charAt(i));
+            }
+            curr.isWord = true;
+        }
+
+        boolean contains(String word) {
+            TrieNode curr = root;
+            for (int i = 0; i < word.length() && curr != null; i++) {
+                curr = curr.children.get(word.charAt(i));
+            }
+            return curr != null && curr.isWord;
+        }
+
+        boolean wordBreak(String word) {
+            int size = word.length();
+            if (size == 0) return true;
+            for (int i = 1; i <= size; i++) {
+                if (contains(word.substring(0, i)) && wordBreak(word.substring(i))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static class TrieNode {
+            Map<Character, TrieNode> children;
+            boolean isWord;
+
+            TrieNode() {
+                children = new HashMap<>();
+                isWord = false;
+            }
+        }
+    }
+}
